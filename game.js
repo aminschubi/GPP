8139ad96aac2856b7d77d2d4e158e9dc53c640ad
Amin Schubhan <amin.schubhan@gmail.com>
2017-10-23 20:20:57 +0200

    big Changes

--- game.js
+++ game.js
@@ -3,11 +3,23 @@ var myState = new Kiwi.State("myState");
 
 myState.preload = function () {
     Kiwi.State.prototype.preload.call(this);
+    this.addSpriteSheet("hammerSprite", "img/hammerMove.png", 162, 120);
 }
 
 myState.create = function(){
     Kiwi.State.prototype.create.call(this);
+    myState.game.stage.resize(1920, 1080);
 
+    this.player = new Player(this, this.textures.hammerSprite, 300, 300,2);
+    this.upKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.W);
+    this.leftKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.A);
+    this.rightKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.D);
+    this.downKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.S);
+    this.spaceKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.SPACEBAR);
+
+    this.player.animation.add("move", [0,1,2,3,4], 0.2, true);
+    this.player.animation.play("move");
+    this.addChild(this.player);
 }
 
 myState.update = function() {
@@ -15,5 +27,9 @@ myState.update = function() {
 
 }
 
+
+
 myGame.states.addState(myState);
 myGame.states.switchState("myState");
+
+
--- player.js
+++ player.js
@@ -1,23 +1,90 @@
-
-
-var Player = function(state, atlas, [x=0], [y=0], [enableInput=false], weaponType){
-
-    Kiwi.GameObjects.Sprite.call(this,state, atlas, [x=0], [y=0], [enableInput=false]);
+var Player = function(state, atlas, x, y, weaponType){
+    Kiwi.GameObjects.Sprite.call(this,state, atlas, x, y, [enableInput=false]);
 
     this.hp = 100;
+    this.facing = "up";
 
-    switch(weaponType){
-        //LongSword
-        case 0:
-            this.movespeedfactor = 1.0;
-            break;
-        //Bow
-        case 1:
-            this.movespeedfactor = 1.1;
-            break;
-        //Hammer
-        case 2:
-            this.movespeedfactor = 0.8;
-            break;
+    //Longsword
+    if(weaponType == 0)
+        this.movespeedfactor = 1.0;
+    //Bow
+    else if(weaponType == 1)
+        this.movespeedfactor = 1.1;
+    //Hammer
+    else if(weaponType == 2)
+        this.movespeedfactor = 0.8;
+
+    
+
+    Player.prototype.update = function(){
+        Kiwi.GameObjects.Sprite.prototype.update.call(this);
+        if(state.downKey.isDown && !state.leftKey.isDown && !state.rightKey.isDown && !state.upKey.isDown){
+            console.log("X");
+            if(this.facing != "down"){
+                this.facing = "down";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(180);
+            }
+            this.transform.y += 2.0*this.movespeedfactor;
+        }
+        else if(state.downKey.isDown && state.leftKey.isDown && !state.rightKey.isDown && !state.upKey.isDown){
+            if(this.facing != "downleft"){
+                this.facing = "downleft";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(225);
+            }
+            this.transform.y += 1.5*this.movespeedfactor;
+            this.transform.x -= 1.5*this.movespeedfactor;
+        }
+        else if(state.downKey.isDown && !state.leftKey.isDown && state.rightKey.isDown && !state.upKey.isDown){
+            if(this.facing != "downright"){
+                this.facing = "downright";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(145);
+            }
+            this.transform.y += 1.5*this.movespeedfactor;
+            this.transform.x += 1.5*this.movespeedfactor;
+        }
+        else if(state.downKey.isDown && !state.leftKey.isDown && !state.rightKey.isDown && state.upKey.isDown){
+            //Nothing Up + Down
+        }
+        else if(!state.downKey.isDown && !state.leftKey.isDown && !state.rightKey.isDown && state.upKey.isDown){
+            if(this.facing != "up"){
+                this.facing = "up";
+                this.rotation = 0;
+            }
+            this.transform.y -= 2*this.movespeedfactor;
+        }
+        else if(!state.downKey.isDown && state.leftKey.isDown && !state.rightKey.isDown && state.upKey.isDown){
+            if(this.facing != "upleft"){
+                this.facing = "upleft";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(315);
+            }
+            this.transform.y -= 1.5*this.movespeedfactor;
+            this.transform.x -= 1.5*this.movespeedfactor;
+        }
+        else if(!state.downKey.isDown && !state.leftKey.isDown && state.rightKey.isDown && state.upKey.isDown){
+            if(this.facing != "upright"){
+                this.facing = "upright";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(45);
+            }
+            this.transform.y -= 1.5*this.movespeedfactor;
+            this.transform.x += 1.5*this.movespeedfactor;
+        }
+        else if(!state.downKey.isDown && !state.leftKey.isDown && state.rightKey.isDown && !state.upKey.isDown){
+            if(this.facing != "right"){
+                this.facing = "right";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(90);
+            }
+            this.transform.x += 2*this.movespeedfactor;
+        }
+        else if(!state.downKey.isDown && state.leftKey.isDown && !state.rightKey.isDown && !state.upKey.isDown){
+            if(this.facing != "left"){
+                this.facing = "left";
+                this.rotation = Kiwi.Utils.GameMath.degreesToRadians(270);
+            }
+            this.transform.x -= 2*this.movespeedfactor;
+        }
+        else if(!state.downKey.isDown && state.leftKey.isDown && state.rightKey.isDown && !state.upKey.isDown){
+            //Nothing Left + Right
+        }
     }
 };
+Kiwi.extend( Player, Kiwi.GameObjects.Sprite );

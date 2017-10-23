12fd127d3fae711c7c13d5e79b6563808a7f7304
Amin Schubhan <amin.schubhan@gmail.com>
2017-10-23 16:19:29 +0200

    second

--- player.js
+++ player.js
@@ -1,5 +1,23 @@
-class Player extends Kiwi.GameObjects.Sprite{
-    constructor(state, atlas, [x=0], [y=0], [enableInput=false]){
-        super(state, atlas, [x=0], [y=0], [enableInput=false]);
+
+
+var Player = function(state, atlas, [x=0], [y=0], [enableInput=false], weaponType){
+
+    Kiwi.GameObjects.Sprite.call(this,state, atlas, [x=0], [y=0], [enableInput=false]);
+
+    this.hp = 100;
+
+    switch(weaponType){
+        //LongSword
+        case 0:
+            this.movespeedfactor = 1.0;
+            break;
+        //Bow
+        case 1:
+            this.movespeedfactor = 1.1;
+            break;
+        //Hammer
+        case 2:
+            this.movespeedfactor = 0.8;
+            break;
     }
-}
+};
